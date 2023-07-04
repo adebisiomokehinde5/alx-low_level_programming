480041ff4dfb869223bc7760b2b7b21079d19807
adebisiomokehinde5 <adebisiomokehinde5@gmail.com>
2023-07-04 15:54:02 +0100

    fixed error 2

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - function used for checking lower cases
+ * @c: is the parameter function
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
