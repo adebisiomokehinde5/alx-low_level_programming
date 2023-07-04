b3311160849c73cf76b93c9929e63e6e9c140265
adebisiomokehinde5 <adebisiomokehinde5@gmail.com>
2023-07-04 17:08:52 +0100

    fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,19 @@
   *
   *Return: void.
   */
+
+
 void print(void)
 {
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
+
+int main()
+{
+    print();
+
+    printf("done\n";)
+
+    return 0;
 }
